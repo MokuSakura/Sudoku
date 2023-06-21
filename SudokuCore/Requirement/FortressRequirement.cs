ec655c4bbb04b171165dfd52d82f31af563c325c
mokusakura <mokusakura.rakushimo@gmail.com>
2023-06-22 02:32:04 +0800

    堡垒数独

---
+++ SudokuCore/Requirement/FortressRequirement.cs
@@ -0,0 +1,94 @@
+using MokuSakura.Sudoku.Core.Coordination;
+using MokuSakura.Sudoku.Core.Game;
+
+namespace MokuSakura.Sudoku.Core.Requirement;
+
+public class FortressConfiguration
+{
+    public List<Coordinate> GreyCoordinates { get; set; } = new();
+}
+public class FortressRequirement : IRequirement<SudokuGame, Coordinate>, IConfigurable<FortressConfiguration>
+{
+    protected HashSet<Coordinate> GreyCoordinates { get; } = new();
+
+    protected static HashSet<Coordinate> GetAdjacentCoordinates(Int32 rowNum, Int32 colNum, Coordinate coordination)
+    {
+        HashSet<Coordinate> res = new();
+        if (coordination.X > 0)
+        {
+            res.Add(new Coordinate(coordination.X - 1, coordination.Y));
+        }
+        // Look down
+        if (coordination.X < rowNum - 1)
+        {
+            res.Add(new Coordinate(coordination.X + 1, coordination.Y));
+        }
+        // Look left
+        if (coordination.Y > 0)
+        {
+            res.Add(new Coordinate(coordination.X, coordination.Y - 1));
+        }
+        // Look right
+        if (coordination.Y < colNum - 1)
+        {
+            res.Add(new Coordinate(coordination.X, coordination.Y + 1));
+        }
+
+        return res;
+    }
+    public Boolean FitRequirement(SudokuGame sudokuGame, Coordinate coordination, Int32 num)
+    {
+        Boolean isGrey = GreyCoordinates.Contains(coordination);
+        HashSet<Coordinate> adjacentCoordinates = GetAdjacentCoordinates(sudokuGame.RowNum, sudokuGame.ColNum, coordination);
+        if (isGrey)
+        {
+            foreach (Coordinate adjacentCoordinate in adjacentCoordinates)
+            {
+                if (GreyCoordinates.Contains(adjacentCoordinate))
+                {
+                    continue;
+                }
+
+                Int32 adjNum = sudokuGame.GetNum(adjacentCoordinate);
+                if (num == 0)
+                {
+                    continue;
+                }
+
+                if (num < adjNum)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        foreach (Coordinate adjacentCoordinate in adjacentCoordinates)
+        {
+            if (!GreyCoordinates.Contains(adjacentCoordinate))
+            {
+                continue;
+            }
+
+            Int32 adjNum = sudokuGame.GetNum(adjacentCoordinate);
+            if (adjNum == 0)
+            {
+                continue;
+            }
+
+            if (adjNum < num)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public void Configure(FortressConfiguration configuration)
+    {
+        GreyCoordinates.UnionWith(configuration.GreyCoordinates);
+    }
+}
